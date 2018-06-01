f9a9195ccb5a8e5366606124ac7538e1c11442be
Shane <strider2112@users.noreply.github.com>
2018-06-01 10:45:29 -0600

    Update UpdateLinks.sh

--- data/UpdateLinks.sh
+++ data/UpdateLinks.sh
@@ -2,8 +2,10 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . "$DIR"/filetransfer.conf
 
-# Update symbolic links
-cp -sR "$linkFromLocationMovies" "$linkToLocationMoves"
-cp -sR "$linkFromLocationTV" "$linkToLocationTV"
+# Update symbolic links, the * is needed to do the files inside the directories
+cp -sR "$linkFromLocationMovies"* "$linkToLocationMoves"
+cp -sR "$linkFromLocationTV"* "$linkToLocationTV"
+
+clear
 
 exit 0
